98e71c48a02c0252f06264cac41c28f0f13f0fac
NatanaelWT <Natanaelhdxd25@gmail.com>
2023-03-10 13:01:39 +0700

    Upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <nav class="flex navbar w-full">
+    <div class="mt-6 w-full">
+      <img src="img/logo-basic.png" alt="Logo" width="120" />
+    </div>
+    <div>
+      <ul class="flex">
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
