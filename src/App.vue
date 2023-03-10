19e4280a5d32c1293008565c01bcb73e619bc09b
NatanaelWT <Natanaelhdxd25@gmail.com>
2023-03-10 13:26:41 +0700

    Post

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <nav class="flex navbar w-full">
     <div class="mt-6 w-full">
-      <img src="img/logo-basic.png" alt="Logo" width="120" />
+      <img src="/img/logo-basic.png" alt="Logo" width="120" />
     </div>
     <div>
       <ul class="flex">
